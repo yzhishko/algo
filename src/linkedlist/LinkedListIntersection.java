6111bdab5ca4beb28be41e88f2cf14efe2f7441f
Viacheslav <xrw.null@gmail.com>
2018-03-04 22:38:27 -0800

    Create LinkedListIntersection.java

---
+++ src/linkedlist/LinkedListIntersection.java
@@ -0,0 +1,28 @@
+
+class LinkedListIntersection {
+
+  
+  /**
+  * https://leetcode.com/problems/intersection-of-two-linked-lists/description/
+  */
+  static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    ListNode a = headA;
+    ListNode b = headB;
+        
+    while(a != b){
+      if(a == null){
+        a = headB;
+      } else {
+        a = a.next;
+      }
+            
+      if(b == null){
+        b = headA;
+      } else {
+        b = b.next;
+      }
+    }
+        
+    return a;
+    }
+}
