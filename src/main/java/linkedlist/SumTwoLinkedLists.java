53144b312a298b7efa62c5bc5138f17e86f5b4fa
Viacheslav <xrw.null@gmail.com>
2018-05-26 22:56:13 -0700

    Update SumTwoLinkedLists.java

--- src/main/java/linkedlist/SumTwoLinkedLists.java
+++ src/main/java/linkedlist/SumTwoLinkedLists.java
@@ -7,15 +7,15 @@
  * }
  */
 class Solution {
-    //https://leetcode.com/problems/add-two-numbers/description/
+    ////https://leetcode.com/problems/add-two-numbers/description/
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         int nextLevel = 0;
         
-        ListNode result = null;
-        ListNode curr = null;
-        
         ListNode currL1 = l1;
+        ListNode prevL1 = l1;
+        
         ListNode currL2 = l2;
+        ListNode prevL2 = l2;
         
         while(true){
             if(currL1 == null && currL2 == null){
@@ -38,35 +38,42 @@ class Solution {
             }
             
             int finalSum = nextLevel == 1 ? sum - 10 : sum;
-            ListNode finalNode = new ListNode(finalSum);
-            if(curr == null){
-                result = finalNode;
-                curr=result;
-            } else {
-                curr.next = finalNode;
-                curr = curr.next;
-            }
             
             if(currL1 != null){
+                currL1.val = finalSum;
+                prevL1 = currL1;
                 currL1 = currL1.next;
+            } else {
+                prevL1.next = new ListNode(finalSum);
+                prevL1 = prevL1.next;
             }
             
             if(currL2 != null){
+                currL2.val = finalSum;
+                prevL2 = currL2;
                 currL2 = currL2.next;
+            } else {
+                prevL2.next = new ListNode(finalSum);
+                prevL2 = prevL2.next;
             }
         }
         
         if(nextLevel == 1){
             ListNode finalNode = new ListNode(1);
-            if(curr == null){
-                result = finalNode;
-                curr = result;
+            if(currL1 == null){
+                prevL1.next = finalNode;
             } else {
-                curr.next = finalNode;
-                curr = curr.next;
+                currL1.next = finalNode;
             }
+            
+            if(currL2 == null){
+                prevL2.next = finalNode;
+            } else {
+                currL2.next = finalNode;
+            }
+            
         }
         
-        return result;
+        return l1;
     }
 }
